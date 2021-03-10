d96cce8ca6e2b28171e3299f86ab84bcc7c27607
Bence Rozsa <robence@gmail.com>
2021-03-10 06:54:26 +0100

    create cupertino app

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // This app is designed only to work vertically, so we limit
+    // orientations to portrait up and down.
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return CupertinoApp(
+      theme: const CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+
+class CupertinoStoreHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text("Cupertino App"),
+        ),
+        child: Center(
+          child: Text("Hello World"),
+        ));
+  }
+}
