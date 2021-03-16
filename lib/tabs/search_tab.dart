a9190fee9a836cca0fb45813e4ceca996f96d98c
Bence Rozsa <robence@gmail.com>
2021-03-16 06:31:41 +0100

    feat(search): add product search

--- lib/tabs/search_tab.dart
+++ lib/tabs/search_tab.dart
@@ -1,5 +1,11 @@
+import 'package:cupertino_store/search_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:cupertino_store/model/app_state_model.dart';
+import 'package:cupertino_store/product_row_item.dart';
+import 'package:cupertino_store/styles.dart';
 
 class SearchTab extends StatefulWidget {
   @override
@@ -7,14 +13,65 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+  late final TextEditingController _controller;
+  late final FocusNode _focusNode;
+  String _terms = '';
+
+  void _onTextChanged() {
+    setState(() {
+      _terms = _controller.text;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController()..addListener(_onTextChanged);
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _buildSearchBox() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: SearchBar(
+        controller: _controller,
+        focusNode: _focusNode,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Search'),
-        )
-      ],
+    final model = Provider.of<AppStateModel>(context);
+    final results = model.search(_terms);
+
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        color: Styles.scaffoldBackground,
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            _buildSearchBox(),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) => ProductRowItem(
+                  product: results[index],
+                  lastItem: index == results.length - 1,
+                ),
+                itemCount: results.length,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
